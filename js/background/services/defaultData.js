b1cb1d4d7e1428afd41f644520941a8835eb29d1
slavikp <slavik.pashanin@gmail.com>
2016-07-29 11:19:57 +0300

    0.1V

---
+++ js/background/services/defaultData.js
@@ -0,0 +1,9 @@
+myApp.constant("defaultData", [
+    {
+        title: "localHost",
+        url: "10.0.0.3"
+    },{
+        title: "Google",
+        url: "https://www.google.se/"
+    }
+]);
---
+++ js/ui/controllers/ConfigCtrl.js
@@ -0,0 +1,11 @@
+
+////////////////////////////////////////////////////////////////////
+// Configuration page controller
+////////////////////////////////////////////////////////////////////
+myApp.controller('ConfigCtrl', function($scope, probeRepo) {
+    $scope.probes = probeRepo.getProbes();
+    $scope.remove = function(id) {
+        probeRepo.removeProbe(id);
+        $scope.probes = probeRepo.getProbes();
+    };
+});
